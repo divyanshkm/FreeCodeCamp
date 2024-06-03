9276c29a3cc3fed96896ab1dfda9eb02fd1c6b69
Divyansh Mahavar <79765685+divyanshkm@users.noreply.github.com>
2024-06-03 21:30:02 +0530

    Color Picker

--- JS DSA/practice/my-react-app/src/App.jsx
+++ JS DSA/practice/my-react-app/src/App.jsx
@@ -1,9 +1,9 @@
-import Counter from "./components/Counter";
+import ColorPicker from "./components/ColorPicker";
 
 function App() {
   return (
     <>
-      <Counter />
+      <ColorPicker />
     </>
   );
 }
--- JS DSA/practice/my-react-app/src/components/MyComponent.jsx
+++ JS DSA/practice/my-react-app/src/components/MyComponent.jsx
@@ -1,30 +1,64 @@
 import React, { useState } from "react";
 
 function MyComponent() {
-  let [name, setName] = useState("guest");
-  const [age, setAge] = useState(22);
-  const [isEmployed, setIsEmployed] = useState(true);
-  const updateName = () => {
-    setName("Dev");
-  };
+  const [name, setName] = useState("superman");
+  const [quantity, setQuantity] = useState(1);
+  const [comment, setComment] = useState("");
+  const [payment, setPayment] = useState("");
+  const [shipping, setShipping] = useState("Delivery");
 
-  const incrementAge = () => {
-    setAge(age + 1);
-  };
+  const handleCommentChange = (event) => setComment(event.target.value);
+  const handleNameChange = (event) => setName(event.target.value);
+  const handleQuantityChange = (event) => setQuantity(event.target.value);
+  const handlePaymentChange = (event) => setPayment(event.target.value);
+  const handleShippingChange = (event) => setShipping(event.target.value);
 
-  const toggleEmployedStatus = () => {
-    setIsEmployed(!isEmployed);
-  };
   return (
     <div>
-      <p>Name: {name}</p>
-      <button onClick={updateName}>Set Name</button>
+      <input className="input" value={name} onChange={handleNameChange}></input>
+      <p className="p-box">Name: {name}</p>
 
-      <p>Age: {age}</p>
-      <button onClick={incrementAge}>Increment Age</button>
+      <input
+        className="input"
+        value={quantity}
+        type="number"
+        onChange={handleQuantityChange}
+      ></input>
+      <p className="p-box">Quantity: {quantity}</p>
 
-      <p>Is Employed?: {isEmployed ? "yes" : "no"}</p>
-      <button onClick={toggleEmployedStatus}>check Employment</button>
+      <textarea
+        className="input"
+        value={comment}
+        placeholder="This is a comment"
+        onChange={handleCommentChange}
+      ></textarea>
+      <p className="p-box">Comment: {comment}</p>
+
+      <select value={payment} onChange={handlePaymentChange}>
+        <option value="">Select an option</option>
+        <option value="Visa">Visa</option>
+        <option value="MasterCard">MasterCard</option>
+        <option value="Giftcard">Gift Card</option>
+      </select>
+      <p>Payment: {payment}</p>
+
+      <input
+        type="radio"
+        value="Pick Up"
+        checked={shipping === "Pick Up"}
+        onChange={handleShippingChange}
+      ></input>
+      <label>Pick up</label>
+      <br />
+
+      <input
+        type="radio"
+        value="Delivery"
+        checked={shipping === "Delivery"}
+        onChange={handleShippingChange}
+      ></input>
+      <label>Delivery</label>
+      <p>Shipping: {shipping}</p>
     </div>
   );
 }
